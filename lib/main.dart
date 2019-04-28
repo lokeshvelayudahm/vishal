b21098a6367480581df000fdc5f19578e3d258fb
VishalVrk <vishalvrk97@gmail.com>
2019-04-28 08:41:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:http/http.dart' as http;
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  File pickedImage;
+  String ocr_text ="";
+  var id=0;
+  bool isImageLoaded = false;
+
+  Future pickImage() async {
+    var tempStore = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      pickedImage = tempStore;
+      isImageLoaded = true;
+    });
+  }
+
+  Future readText() async {
+    FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);
+    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
+    VisionText readText = await recognizeText.processImage(ourImage);
+   
+
+    for (TextBlock block in readText.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement word in line.elements) {
+          // print(word.text);
+          ocr_text=" "+word.text;
+          // id=id+1;
+        }
+      }
+    }  print(ocr_text);
+     
+    var url = 'http://10.1.122.255:5000';
+    var response = await http.post(url, body: {'txt': ocr_text});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    print(await http.read('http://10.1.122.255:5000'));
+  }
+
+  Future decode() async {
+    FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);
+    BarcodeDetector barcodeDetector = FirebaseVision.instance.barcodeDetector();
+    List barCodes = await barcodeDetector.detectInImage(ourImage);
+
+    for (Barcode readableCode in barCodes) {
+      print(readableCode.displayValue);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: <Widget>[
+        SizedBox(height: 100.0),
+        isImageLoaded
+            ? Center(
+                child: Container(
+                    height: 200.0,
+                    width: 200.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: FileImage(pickedImage), fit: BoxFit.cover))),
+              )
+            : Container(),
+        SizedBox(height: 10.0),
+        RaisedButton(
+          child: Text('Pick an image'),
+          onPressed: pickImage,
+        ),
+        SizedBox(height: 10.0),
+        RaisedButton(
+          child: Text('Read Text'),
+          onPressed: readText,
+        ),
+      ],
+    ));
+  }
+}
